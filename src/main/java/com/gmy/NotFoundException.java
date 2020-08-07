ef9aa15512d4b886ad68d65b3b2d449a76154cb8
Kevin Guo <44527463+KevinMuyaoGuo@users.noreply.github.com>
2020-08-07 10:51:49 +0800

    实现框架搭建和日志处理

---
+++ src/main/java/com/gmy/NotFoundException.java
@@ -0,0 +1,24 @@
+package com.gmy;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @Description
+ * @Package com.gmy
+ * @Author 20803848
+ * @Date 2020/8/7
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException{
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/gmy/web/IndexController.java
@@ -0,0 +1,28 @@
+package com.gmy.web;
+
+
+import com.gmy.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @Description
+ * @Package com.gmy.web
+ * @Author 20803848
+ * @Date 2020/8/7
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
+//        int i = 9/0;
+//        String blog = null;
+//        if (blog == null) {
+//            throw new NotFoundException("博客不存在");
+//        }
+        System.out.println("---------index----------");
+        return "index";
+    }
+}
