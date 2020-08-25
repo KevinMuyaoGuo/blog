f7352bad255696777dd05e27af3818a18f8ff5b6
KevinGuo <kevinguomy@163.com>
2020-08-25 20:30:10 +0800

    实现文章归档页面展示功能

--- src/main/java/com/gmy/service/BlogService.java
+++ src/main/java/com/gmy/service/BlogService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Kevin Guo
@@ -34,6 +35,10 @@ public interface BlogService {
     Page<Blog> listBlog(String query, Pageable pageable);
 
     List<Blog> listRecommendBlogTop(Integer size);
+
+    Map<String, List<Blog>> archiveBlog();
+
+    Long countBlog();
 
     //修改
     Blog updateBlog(Long id, Blog blog);
---
+++ src/main/java/com/gmy/web/ArchiveShowController.java
@@ -0,0 +1,28 @@
+package com.gmy.web;
+
+import com.gmy.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Kevin Guo
+ * @package com.gmy.web
+ * @date 2020/8/25 19:39
+ * @description
+ */
+@Controller
+public class ArchiveShowController {
+
+    @Autowired
+    private BlogService blogService;
+
+    @GetMapping("/archives")
+    public String archives(Model model) {
+        model.addAttribute("archiveMap", blogService.archiveBlog());
+        model.addAttribute("blogCount", blogService.countBlog());
+        return "archives";
+    }
+
+}
