67bc493a51636ef87881b53932a842318ba26336
KevinGuo <kevinguomy@163.com>
2020-09-01 10:47:14 +0800

    修复归档页显示未发布文章bug

--- src/main/java/com/gmy/dao/BlogRepository.java
+++ src/main/java/com/gmy/dao/BlogRepository.java
@@ -36,6 +36,6 @@ public interface BlogRepository extends JpaRepository<Blog, Long>, JpaSpecificat
     @Query("select function('date_format', b.updateTime, '%Y') as year from t_blog b group by year order by function('date_format', b.updateTime, '%Y') desc")
     List<String> findGroupYear();
 
-    @Query("select b from t_blog b where function('date_format',b.updateTime,'%Y') = ?1")
+    @Query("select b from t_blog b where function('date_format',b.updateTime,'%Y') = ?1 and b.published = true")
     List<Blog> findByYear(String year);
 }
